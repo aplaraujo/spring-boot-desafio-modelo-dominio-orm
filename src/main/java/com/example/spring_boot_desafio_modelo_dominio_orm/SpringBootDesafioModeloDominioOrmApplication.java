a6ed8c328d6759fd93eee9f73e9134b225d35f40
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-08-28 11:43:34 -0300

    first commit

---
+++ src/main/java/com/example/spring_boot_desafio_modelo_dominio_orm/SpringBootDesafioModeloDominioOrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_desafio_modelo_dominio_orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDesafioModeloDominioOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDesafioModeloDominioOrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_desafio_modelo_dominio_orm/SpringBootDesafioModeloDominioOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_desafio_modelo_dominio_orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDesafioModeloDominioOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
